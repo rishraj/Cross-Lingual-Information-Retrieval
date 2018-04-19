b7c28cc77a91fdb75d878ccaa0b3745ee58d432b
Rishav Raj <rishraj@iitk.ac.in>
2018-04-20 00:34:51 +0530

    Update ir.py

--- ir.py
+++ ir.py
@@ -5,15 +5,26 @@ import importlib
 moduleName = 'hi_to_eng'
 importlib.import_module(moduleName)
 from hi_to_eng import transliterate,_setup
+import re
 
 
 f = open('/home/rishav/Desktop/hi-topics.txt', 'r')
 read_file = f.read()
 
+f = open('/home/rishav/Desktop/crowd_transliterations.hi-en.txt', 'r')
+transliterate_file = f.read()
+
 # with open('/home/rishav/Desktop/hi-en_dict.csv', 'r') as f_obj:
 #     read_dict = csv.reader(f_obj)
 
+trans_dict = {}
+for line in transliterate_file.splitlines():
+    # print(line)
+    dict_list = re.split(r'\s{1,}', line)
+    # print(dict_list)
+    trans_dict.update({dict_list[1]: dict_list[0]})
 
+# print(trans_dict['कोप'])
 
 soup = BeautifulSoup(read_file, "html5lib")
 
@@ -44,20 +55,22 @@ for query in queries.splitlines():
     eng_query = ""
     word_list = query.split(' ')
     for word in word_list:
-        if word in dict:
-            # eng_query = eng_query+'('+word+')'+dict[word][0]+' '
-            if len(dict[word]) == 1:
-                eng_query = eng_query+dict[word][0]+' '
-            else:
-                eng_query = eng_query+dict[word][0]+' '
-            # print(word, ' ', dict[word],' ')
+        if (word in trans_dict) or (word in dict):
+            if word in trans_dict:
+                eng_query = eng_query+trans_dict[word]+' '
+            if word in dict:
+                if len(dict[word]) == 1:
+                    eng_query = eng_query+dict[word][0]+' '
+                else:
+                    # for i in range(len(dict[word])):
+                    eng_query = eng_query+dict[word][0]+' '
         else:
-            eng_query = eng_query+transliterate(word, 'devanagari', 'iast')+' '
+            eng_query = eng_query+(transliterate(word, 'devanagari', 'hk')).lower()+' '
     print(query)
     print(eng_query)
     eng_queries = eng_queries+eng_query+'\n'
 
-f = open("/home/rishav/Desktop/en-queries.txt", "w+")
+f = open("/home/rishav/Desktop/expanded-queries.txt", "w+")
 f.write("<topics>\n")
 num = 76
 for line in eng_queries.splitlines():
